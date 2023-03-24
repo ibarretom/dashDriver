bf62a91e51b362eb225e3cb31946a4573c96b24e
ibarretom <igorbm.23@gmail.com>
2023-03-24 20:03:51 -0300

    Criação de abastecimento linkada a api

--- src/views/Fuel.jsx
+++ src/views/Fuel.jsx
@@ -1,73 +1,100 @@
-import { StyleSheet, View, Text, ScrollView, Alert, TouchableWithoutFeedback, Keyboard } from "react-native"
-import { useEffect, useState } from "react"
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native'
+import { useEffect, useState } from 'react'
 
-import { AppButton } from "../components/buttons/AppButton"
-import { BackButton } from "../components/buttons/BackButton"
-import { AppTextInput } from "../components/inputs/AppTextInput"
-import { AppDatePicker } from "../components/inputs/AppDatePicker"
+import { AppButton } from '../components/buttons/AppButton'
+import { BackButton } from '../components/buttons/BackButton'
+import { AppTextInput } from '../components/inputs/AppTextInput'
+import { AppDatePicker } from '../components/inputs/AppDatePicker'
+import { AppPicker } from '../components/inputs/AppPicker'
 
-import { useForm, Controller } from "react-hook-form"
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup"
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
-import { db, addDoc, collection } from "../plugins/firebase"
-import { useAuth } from "../hooks/auth"
-import { months } from "../utils/months"
+import fuel from '../service/spent/fuel/fuel.service'
 
 const fuelSchema = yup.object({
+  type: yup.string().required('Escolha o tipo'),
   liters: yup
     .number()
-    .transform((value) => (isNaN(value) || value === null || value === undefined) ? 0 : value)
+    .transform((value) =>
+      isNaN(value) || value === null || value === undefined ? 0 : value
+    )
     .positive('Este campo deve maior que zero')
     .required('Digite o valor'),
   amount: yup
     .number()
-    .transform((value) => (isNaN(value) || value === null || value === undefined) ? 0 : value)
+    .transform((value) =>
+      isNaN(value) || value === null || value === undefined ? 0 : value
+    )
     .positive('Este campo deve maior que zero')
     .required('Digite o valor'),
-  date: yup.string().required('Digite a data')
+  date: yup.string().required('Digite a data'),
 })
 
 export function Fuel({ navigation }) {
-  const { user } = useAuth()
-
   const [isLoading, setIsLoading] = useState(false)
 
-  const { control, handleSubmit, reset, formState: { errors, isSubmitSuccessful } } = useForm({
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({
     defaultValues: {
+      type: 'gas_natural',
       liters: 0,
       amount: 0,
-      date: `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`,
+      date: `${new Date().getDate()}/${
+        new Date().getMonth() + 1
+      }/${new Date().getFullYear()}`,
     },
-    resolver: yupResolver(fuelSchema)
+    resolver: yupResolver(fuelSchema),
   })
 
   useEffect(() => {
     if (isSubmitSuccessful) reset()
   }, [isSubmitSuccessful])
 
-  async function handleFuelSubmit({ liters, amount, date }) {
+  async function handleFuelSubmit({ type, liters, amount, date }) {
     setIsLoading(true)
 
+    const date_object = {
+      day: parseInt(date.split('/')[0]),
+      month: parseInt(date.split('/')[1]) - 1, // menos 1 porque no javascript o mes começa de 0 e o date picker de 1
+      year: parseInt(date.split('/')[2]),
+    }
     try {
-      await addDoc(collection(db, 'fuel'), {
-        user_id: user.uid,
+      await fuel.create({
+        type,
         amount,
         liters,
-        date: {
-          month_name: months[date.split('/')[1] - 1].label, // menos 1 porque no javascript o mes começa de 0 e o date picker de 1
-          day: parseInt(date.split('/')[0]),
-          month: parseInt(date.split('/')[1]) - 1, // menos 1 porque no javascript o mes começa de 0 e o date picker de 1
-          year: parseInt(date.split('/')[2]),
-          date_string: date,
-        },
+        fuel_date: new Date(
+          date_object.year,
+          date_object.month,
+          date_object.day
+        ),
       })
-      Alert.alert('Abastecimento adicionado', 'Abastecimento adicionado com sucesso')
+
+      Alert.alert(
+        'Abastecimento adicionado',
+        'Abastecimento adicionado com sucesso'
+      )
 
       setIsLoading(false)
-
     } catch (e) {
-      Alert.alert('Erro ao adicionar abastecimento', 'Não foi possível abastecimento')
+      Alert.alert(
+        'Erro ao adicionar abastecimento',
+        'Não foi possível adicionar abastecimento'
+      )
 
       setIsLoading(false)
     }
@@ -88,10 +115,28 @@ export function Fuel({ navigation }) {
         <ScrollView>
           <Controller
             control={control}
+            name={'type'}
+            render={({ field: { onChange, value } }) => (
+              <AppPicker
+                label={'Combustível'}
+                items={[
+                  { label: 'Gás natural (GNV)', value: 'gas_natural' },
+                  { label: 'Gasolina', value: 'gasolina' },
+                  { label: 'Etanol', value: 'etanol' },
+                ]}
+                value={value}
+                setValue={onChange}
+                error={errors.type?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
             name={'liters'}
             render={({ field: { onChange, value } }) => (
               <AppTextInput
-                label={'Quantidade em litros'}
+                label={'Quantidade'}
                 placeholder={'Litros'}
                 inputMode={'numeric'}
                 keyboardType={'numeric'}
@@ -127,7 +172,13 @@ export function Fuel({ navigation }) {
                 value={value}
                 setValue={onChange}
                 error={errors.date?.message}
-                maximumDate={new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())}
+                maximumDate={
+                  new Date(
+                    new Date().getFullYear(),
+                    new Date().getMonth(),
+                    new Date().getDate()
+                  )
+                }
               />
             )}
           />
@@ -141,7 +192,11 @@ export function Fuel({ navigation }) {
             />
           </View>
           <View style={{ marginTop: 8 }}>
-            <AppButton title={'Cancelar'} color={'#D6561A'} onPress={navigateBack} />
+            <AppButton
+              title={'Cancelar'}
+              color={'#D6561A'}
+              onPress={navigateBack}
+            />
           </View>
 
           <View style={styles.emptySpace}></View>
@@ -154,18 +209,18 @@ export function Fuel({ navigation }) {
 const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: '600',
-    marginLeft: 8
+    marginLeft: 8,
   },
   addressText: {
     fontWeight: '300',
-    marginBottom: 8
+    marginBottom: 8,
   },
   emptySpace: {
-    marginTop: 120
-  }
+    marginTop: 120,
+  },
 })
