ec6873fdb111bd358706a82b7cf6307957d71d4f
ibarretom <igorbm.23@gmail.com>
2023-03-24 22:06:50 -0300

    Implementação do serviço de despesa no submit do formulário

--- src/views/Spent.jsx
+++ src/views/Spent.jsx
@@ -1,73 +1,81 @@
-import { useEffect, useState } from "react"
-import { StyleSheet, View, Text, ScrollView, Alert } from "react-native"
+import { useEffect, useState } from 'react'
+import { StyleSheet, View, Text, ScrollView, Alert } from 'react-native'
 
-import { AppButton } from "../components/buttons/AppButton"
-import { BackButton } from "../components/buttons/BackButton"
-import { AppTextInput } from "../components/inputs/AppTextInput"
-import { AppDatePicker } from "../components/inputs/AppDatePicker"
-import { AppPicker } from "../components/inputs/AppPicker"
+import { AppButton } from '../components/buttons/AppButton'
+import { BackButton } from '../components/buttons/BackButton'
+import { AppTextInput } from '../components/inputs/AppTextInput'
+import { AppDatePicker } from '../components/inputs/AppDatePicker'
+import { AppPicker } from '../components/inputs/AppPicker'
 
-import { useForm, Controller } from "react-hook-form"
-import { yupResolver } from "@hookform/resolvers/yup"
-import * as yup from "yup"
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
-import { db, addDoc, collection } from "../plugins/firebase"
-import { useAuth } from "../hooks/auth"
-import { months } from "../utils/months"
+import spent from '../service/spent/spent/spent.service'
 
 const spendSchema = yup.object({
   spentType: yup.string().required('Insira o tipo de gasto'),
   amount: yup
     .number()
-    .transform((value) => (isNaN(value) || value === null || value === undefined) ? 0 : value)
+    .transform((value) =>
+      isNaN(value) || value === null || value === undefined ? 0 : value
+    )
     .positive('Este campo deve maior que zero')
     .required('Digite o valor'),
-  date: yup.string().required('Insira a data')
+  date: yup.string().required('Insira a data'),
 })
 
 export function Spent({ navigation }) {
-  const { user } = useAuth()
-
   const [isLoading, setIsLoading] = useState()
 
-  const { control, handleSubmit, reset, formState: { errors, isSubmitSuccessful } } = useForm({
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({
     defaultValues: {
       spentType: 'aluguel_de_carro',
       amount: 0,
-      date: `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`,
-      description: ''
+      date: `${new Date().getDate()}/${
+        new Date().getMonth() + 1
+      }/${new Date().getFullYear()}`,
     },
-    resolver: yupResolver(spendSchema)
+    resolver: yupResolver(spendSchema),
   })
 
   useEffect(() => {
     if (isSubmitSuccessful) reset()
   }, [isSubmitSuccessful])
 
-  async function handleSpentSubmit({ spentType, amount, date, description }) {
+  async function handleSpentSubmit({ spentType, amount, date }) {
     setIsLoading(true)
 
+    const date_object = {
+      day: parseInt(date.split('/')[0]),
+      month: parseInt(date.split('/')[1]) - 1, // menos 1 porque no javascript o mes começa de 0 e o date picker de 1
+      year: parseInt(date.split('/')[2]),
+    }
+
     try {
-      await addDoc(collection(db, 'spent'), {
-        user_id: user.uid,
+      await spent.create({
         spent_type: spentType,
         amount,
-        date: {
-          month_name: months[date.split('/')[1] - 1].label, // menos 1 porque no javascript o mes começa de 0 e o date picker de 1
-          day: parseInt(date.split('/')[0]),
-          month: parseInt(date.split('/')[1]) - 1, // menos 1 porque no javascript o mes começa de 0 e o date picker de 1
-          year: parseInt(date.split('/')[2]),
-          date_string: date,
-        },
-        description
+        spent_date: new Date(
+          date_object.year,
+          date_object.month,
+          date_object.day
+        ).toISOString(),
       })
 
-      Alert.alert('Despesa adicionada', 'Despesa adicionanda com sucesso')
+      Alert.alert('Despesa adicionada', 'Despesa adicionada com sucesso')
 
       setIsLoading(false)
-
     } catch (e) {
-      Alert.alert('Erro ao adicionar despesa', 'Não foi possível adicionar despesa')
+      Alert.alert(
+        'Erro ao adicionar despesa',
+        'Não foi possível adicionar despesa'
+      )
 
       setIsLoading(false)
     }
@@ -95,7 +103,7 @@ export function Spent({ navigation }) {
                 { label: 'Aluguel de carro', value: 'aluguel_de_carro' },
                 { label: 'Manutenção', value: 'manutencao' },
                 { label: 'Almoço', value: 'almoco' },
-                { label: 'Outros', value: 'outros' }
+                { label: 'Outros', value: 'outros' },
               ]}
               value={value}
               setValue={onChange}
@@ -129,20 +137,13 @@ export function Spent({ navigation }) {
               value={value}
               setValue={onChange}
               error={errors.date?.message}
-              maximumDate={new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name={'description'}
-          render={({ field: { onChange, value } }) => (
-            <AppTextInput
-              label={'Descrição'}
-              placeholder={'Descrição'}
-              onChange={onChange}
-              value={value}
+              maximumDate={
+                new Date(
+                  new Date().getFullYear(),
+                  new Date().getMonth(),
+                  new Date().getDate()
+                )
+              }
             />
           )}
         />
@@ -156,7 +157,11 @@ export function Spent({ navigation }) {
           />
         </View>
         <View style={{ marginTop: 8 }}>
-          <AppButton title={'Cancelar'} color={'#D6561A'} onPress={navigateBack} />
+          <AppButton
+            title={'Cancelar'}
+            color={'#D6561A'}
+            onPress={navigateBack}
+          />
         </View>
 
         <View style={styles.emptySpace}></View>
@@ -168,18 +173,18 @@ export function Spent({ navigation }) {
 const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: '600',
-    marginLeft: 8
+    marginLeft: 8,
   },
   addressText: {
     fontWeight: '300',
-    marginBottom: 8
+    marginBottom: 8,
   },
   emptySpace: {
-    marginTop: 120
-  }
+    marginTop: 120,
+  },
 })
