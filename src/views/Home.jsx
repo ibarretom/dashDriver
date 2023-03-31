6c05026268994e13ca83942e7dd32875559b5ed0
ibarretom <igorbm.23@gmail.com>
2023-03-30 21:30:55 -0300

    Implementação do serviço de renda mensal e informações do carrod

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,65 +1,113 @@
-import { useEffect, useState } from "react";
-import { ActivityIndicator, ScrollView, StyleSheet, Text, View } from "react-native"
+import { useEffect, useState } from 'react'
+import {
+  ActivityIndicator,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
 
-import { AppSquareWithIconButton } from "../components/buttons/AppSquareWithIconButton"
-import { AppPicker } from "../components/inputs/AppPicker"
-import { RevenueCard } from "../components/RevenueCard"
-import { Picture } from "../components/Picture"
-import { CarInfo } from "../components/CarInfo"
-import { Chart } from "../components/Chart"
+import { AppSquareWithIconButton } from '../components/buttons/AppSquareWithIconButton'
+import { AppPicker } from '../components/inputs/AppPicker'
+import { RevenueCard } from '../components/RevenueCard'
+import { Picture } from '../components/Picture'
+import { CarInfo } from '../components/CarInfo'
+import { Chart } from '../components/Chart'
 
-import { months } from "../utils/months"
+import { months } from '../utils/months'
 
-import { useRevenue } from "../hooks/revenue"
-import { useCarInfo } from "../hooks/carInfo"
-import { useNavigationState } from "@react-navigation/native";
-import { useAuth } from "../hooks/auth";
+import { useNavigationState } from '@react-navigation/native'
+import { useAuth } from '../hooks/auth'
+
+import Revenue from '../service/revenue/revenue.service'
+import carInfoService from '../service/carInfo/carInfo.service'
 
 export function Home({ navigation }) {
   const { user } = useAuth()
 
-  const { getRevenue, getTotalRevenue } = useRevenue()
-  const { getCarInfo } = useCarInfo()
-  const state = useNavigationState(state => state);
+  const state = useNavigationState((state) => state)
 
-  const [resumeMonth, setResumeMonth] = useState(months[new Date().getMonth()].label)
+  const [resumeMonth, setResumeMonth] = useState(new Date().getMonth())
   const [revenue, setRevenue] = useState({
     earnings: [
       { label: 'Corridas', value: 25 },
       { label: 'Outros', value: 25 },
     ],
-    spents: [
+    spent: [
       { label: 'Aluguel de carro', value: 25, color: '#6085F7' },
       { label: 'Combustível', value: 12, color: '#0E6ECE' },
-      { label: 'Munutenção', value: 20, color: '#2D9CED' },
+      { label: 'Manutenção', value: 20, color: '#2D9CED' },
       { label: 'Almoço', value: 30, color: '#68CEEE' },
       { label: 'Outros', value: 40, color: '#FACA9B' },
-    ]
+    ],
+    total: [{ label: 'Total', value: 0 }],
   })
-  const [totalRevenue, setTotalRevenue] = useState(0)
+
   const [carInfo, setCarInfo] = useState({
     kilometers: 0,
-    liters: 0,
-    kilometersPerLitters: 0,
-    realsPerLiter: 0
+    fuel: {
+      gas: 0,
+      etanol: 0,
+      gasoline: 0,
+    },
+    total_amount_fuel: 0,
   })
   const [loadingScreen, setLoadingScreen] = useState(false)
 
   useEffect(() => {
-    getRevenue(resumeMonth)
-      .then(d => {
+    Revenue.get({
+      date: new Date(
+        new Date().getFullYear(),
+        resumeMonth,
+        new Date().getDate()
+      ),
+    })
+      .then((d) => {
+        d.spent = d.spent.map((s) => {
+          if (s.label == 'Aluguel de carro') {
+            return {
+              ...s,
+              color: '#6085F7',
+            }
+          } else if (s.label == 'Combustível') {
+            return {
+              ...s,
+              color: '#0E6ECE',
+            }
+          } else if (s.label == 'Manutenção') {
+            return {
+              ...s,
+              color: '#2D9CED',
+            }
+          } else if (s.label == 'Almoço') {
+            return {
+              ...s,
+              color: '#68CEEE',
+            }
+          } else if (s.label == 'Outros') {
+            return {
+              ...s,
+              color: '#FACA9B',
+            }
+          }
+        })
         setRevenue(d)
       })
-      .catch(e => console.log('home', e.message))
+      .catch((e) => console.log('home', e.message))
 
-    getCarInfo(resumeMonth)
-      .then(d => setCarInfo(d))
-      .catch(e => console.log('home', e.message))
+    carInfoService
+      .get({
+        date: new Date(
+          new Date().getFullYear(),
+          resumeMonth,
+          new Date().getDate()
+        ),
+      })
+      .then((d) => {
+        setCarInfo(d)
+      })
+      .catch((e) => console.log('home', e.message))
   }, [resumeMonth, state])
-
-  useEffect(() => {
-    setTotalRevenue(getTotalRevenue(revenue))
-  }, [revenue])
 
   function navigate({ page }) {
     navigation.navigate(page)
@@ -69,45 +117,93 @@ export function Home({ navigation }) {
     <ScrollView style={styles.mainContainer}>
       <>
         <View style={styles.welcomeContainer}>
-          <Picture setLoadingScreen={setLoadingScreen} userImage={user?.photoURL}/>
+          <Picture
+            setLoadingScreen={setLoadingScreen}
+            userImage={user?.photo_url}
+          />
           <View style={styles.welcomeTextContainer}>
-            <Text style={styles.welcomeText}>Bem-vindo, {user?.displayName}</Text>
-            <Text>{`${new Date().getDate()} de ${months[new Date().getMonth()].label} de ${new Date().getFullYear()}`} </Text>
+            <Text style={styles.welcomeText}>Bem-vindo, {user?.name}</Text>
+            <Text>
+              {`${new Date().getDate()} de ${
+                months[new Date().getMonth()].label
+              } de ${new Date().getFullYear()}`}{' '}
+            </Text>
           </View>
         </View>
 
         <Text style={styles.title}>Adicionar</Text>
         <ScrollView style={styles.buttonsContainer} horizontal>
-          <AppSquareWithIconButton iconName={'directions-car'} title={'Corrida'} onPress={() => navigate({ page: 'CarRide' })} />
-          <AppSquareWithIconButton iconName={'local-gas-station'} title={'Abastecimento'} onPress={() => navigate({ page: 'Fuel' })} />
-          <AppSquareWithIconButton iconName={'speed'} title={'Kilometragem'} onPress={() => navigate({ page: 'Kilometers' })} />
-          <AppSquareWithIconButton iconName={'attach-money'} title={'Ganho'} onPress={() => navigate({ page: 'Earning' })} />
-          <AppSquareWithIconButton iconName={'money-off'} title={'Despesa'} onPress={() => navigate({ page: 'Spent' })} />
+          <AppSquareWithIconButton
+            iconName={'directions-car'}
+            title={'Corrida'}
+            onPress={() => navigate({ page: 'CarRide' })}
+          />
+          <AppSquareWithIconButton
+            iconName={'local-gas-station'}
+            title={'Abastecimento'}
+            onPress={() => navigate({ page: 'Fuel' })}
+          />
+          <AppSquareWithIconButton
+            iconName={'speed'}
+            title={'Quilometragem'}
+            onPress={() => navigate({ page: 'Kilometers' })}
+          />
+          <AppSquareWithIconButton
+            iconName={'attach-money'}
+            title={'Ganho'}
+            onPress={() => navigate({ page: 'Earning' })}
+          />
+          <AppSquareWithIconButton
+            iconName={'money-off'}
+            title={'Despesa'}
+            onPress={() => navigate({ page: 'Spent' })}
+          />
         </ScrollView>
 
         <View>
           <Text style={styles.title}>Resumo</Text>
-          <AppPicker value={resumeMonth} setValue={setResumeMonth} items={months} />
+          <AppPicker
+            value={resumeMonth}
+            setValue={setResumeMonth}
+            items={months}
+          />
         </View>
-        <Chart data={revenue.spents} />
+        <Chart data={revenue.spent} />
 
         <Text style={styles.title}>Ganhos</Text>
         <View>
-          {revenue.earnings.map((d, i) => <RevenueCard key={i} title={d.label} value={d.value.toFixed(2)} color={"#04D64A"} />)}
+          {revenue.earnings.map((d, i) => (
+            <RevenueCard
+              key={i}
+              title={d.label}
+              value={d.value.toFixed(2)}
+              color={'#04D64A'}
+            />
+          ))}
         </View>
 
         <Text style={styles.title}>Despesas</Text>
         <View>
-          {revenue.spents.map((d, i) => <RevenueCard key={i} title={d.label} value={d.value.toFixed(2)} color={"#D6561A"} />)}
+          {revenue.spent.map((d, i) => (
+            <RevenueCard
+              key={i}
+              title={d.label}
+              value={d.value.toFixed(2)}
+              color={'#D6561A'}
+            />
+          ))}
         </View>
 
         <Text style={styles.title}>Total</Text>
         <View>
-          <RevenueCard
-            title={"Mês de " + months[new Date().getMonth()].label.toLowerCase()}
-            value={totalRevenue.toFixed(2)}
-            color={"#0E6ECE"}
-          />
+          {revenue.total.map((d, i) => (
+            <RevenueCard
+              key={i}
+              title={'Mês de ' + months[resumeMonth].label.toLowerCase()}
+              value={d.value.toFixed(2)}
+              color={'#0E6ECE'}
+            />
+          ))}
         </View>
 
         <View
@@ -115,32 +211,52 @@ export function Home({ navigation }) {
             borderBottomColor: 'E4E5E7',
             borderBottomWidth: StyleSheet.hairlineWidth,
             marginTop: 24,
-            marginBottom: 12
+            marginBottom: 12,
           }}
         />
 
         <Text style={styles.title}>Informações do carro</Text>
         <View style={styles.carInfoContent}>
-          <CarInfo value={carInfo.kilometers.toFixed(1)} unit={"Km"} />
+          <CarInfo value={carInfo.kilometers.toFixed(1)} unit={'Km'} />
 
-          <CarInfo value={carInfo.kilometersPerLitters.toFixed(2)} unit={"Km/L"} />
-
-          <CarInfo value={carInfo.realsPerLiter.toFixed(2)} unit={"R$/L"} />
+          <CarInfo
+            value={
+              carInfo.fuel.gasoline + carInfo.fuel.etanol != 0
+                ? (
+                    carInfo.kilometers /
+                    (carInfo.fuel.gasoline + carInfo.fuel.etanol)
+                  ).toFixed(1)
+                : '0,0'
+            }
+            unit={'Km/L'}
+          />
+          <CarInfo
+            value={
+              carInfo.fuel.gas != 0
+                ? (carInfo.kilometers / carInfo.fuel.gas).toFixed(1)
+                : '0,0'
+            }
+            unit={'Km/m³'}
+          />
+        </View>
+        <View style={styles.carInfoContent}>
+          <CarInfo
+            value={
+              carInfo.total_amount_fuel != 0
+                ? (carInfo.total_amount_fuel / carInfo.kilometers).toFixed(1)
+                : '0,0'
+            }
+            unit={'R$/Km'}
+          />
         </View>
 
         <View style={[styles.emptyspace]}></View>
 
-        {
-          loadingScreen &&
-          <View
-            style={[
-              StyleSheet.absoluteFill,
-              styles.uploadingOverlay
-            ]}
-          >
-            <ActivityIndicator color='#fff' animating size='large' />
+        {loadingScreen && (
+          <View style={[StyleSheet.absoluteFill, styles.uploadingOverlay]}>
+            <ActivityIndicator color="#fff" animating size="large" />
           </View>
-        }
+        )}
       </>
     </ScrollView>
   )
@@ -153,30 +269,31 @@ const styles = StyleSheet.create({
   welcomeContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 8
+    marginBottom: 8,
   },
   welcomeTextContainer: {
-    marginLeft: 16
+    marginLeft: 16,
   },
   welcomeText: {
     fontSize: 20,
-    fontWeight: '600'
+    fontWeight: '600',
   },
   title: {
     fontSize: 18,
     fontWeight: '700',
-    marginTop: 16
+    marginTop: 16,
   },
   buttonsContainer: {
-    marginTop: 8
+    marginTop: 8,
   },
   carInfoContent: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginTop: 8
+    flexWrap: 'wrap',
+    marginTop: 8,
   },
   emptyspace: {
-    marginBottom: 52
+    marginBottom: 52,
   },
   uploadingOverlay: {
     backgroundColor: 'rgba(0,0,0,0.4)',
